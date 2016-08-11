f063d1d17a15650c76d0c5517a8bc8d9c329d65e
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-11 14:54:59 -0400

    use `file-is-binary`

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 'use strict';
 
 var through = require('through2');
-var isBinary = require('isbinaryfile');
+var isBinary = require('file-is-binary');
 
 module.exports = function(options) {
   return through.obj(function(file, enc, next) {
 
     file.isBinary = function() {
-      if (file.hasOwnProperty('_isBinary')) {
-        return file._isBinary;
-      }
-
-      if (file.isNull() || file.isStream() || file.isDirectory()) {
-        file._isBinary = false;
-        return false;
-      }
-
-      var len = file.stat ? file.stat.size : file.contents.length;
-      file._isBinary = isBinary.sync(file.contents, len);
-      return file._isBinary;
+      return isBinary(file);
     };
 
     next(null, file);
